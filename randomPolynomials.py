686f63578a42115b22bf98025a1db1b820ac9f26
nbliss <nbliss2@uic.edu>
2017-04-09 23:09:21 -0500

    can now give a weight, for changing density

--- randomPolynomials.py
+++ randomPolynomials.py
@@ -6,8 +6,9 @@ def binom(n,k):
             prod /= i
     return prod
 
-from random import randint
+from random import randint,random
 from TestAllPolys import GenerateAllMonomials,convertSupportToPoly
+'''
 #-------------------------------------------------------------------------------
 def randomPolySupports(DegreeBound, Dimension, numPolys=1):
     """
@@ -16,6 +17,7 @@ def randomPolySupports(DegreeBound, Dimension, numPolys=1):
     the power set of all monomials, so if there are M of them, we pick
     a random int 0<=x<=M and use its binary representation to choose
     from the list of monomials. Can get several random polys at a time.
+    Weight is the probability that a monomial gets chosen.
     """
     numberMonoms = int(binom(Dimension + DegreeBound, Dimension))
     def getPowerSetIndex():
@@ -33,6 +35,29 @@ def randomPolySupports(DegreeBound, Dimension, numPolys=1):
     # else return all of them
     if len(toReturn)==1:return toReturn[0] 
     return toReturn
+'''
+
+#-------------------------------------------------------------------------------
+def randomPolySupports(DegreeBound, Dimension, numPolys=1, weight=0.5):
+    """
+    Generates random polynomials in Dimension variables and total
+    degree <= DegreeBound. Equivalent to finding a random element of
+    the power set of all monomials, so if there are M of them, we pick
+    a random int 0<=x<=M and use its binary representation to choose
+    from the list of monomials. Can get several random polys at a time.
+    Weight is the probability that a monomial gets chosen.
+    """
+    numberMonoms = int(binom(Dimension + DegreeBound, Dimension))
+    monoms = GenerateAllMonomials(DegreeBound, Dimension)
+    toReturn = [[] for i in xrange(numPolys)]
+    for monom in monoms:
+        for polyNum in xrange(numPolys):
+            if random()<weight:toReturn[polyNum].append(monom)
+    # if we just wanted one, return it,
+    # else return all of them
+    if len(toReturn)==1:return toReturn[0] 
+    return toReturn
+
 
 if __name__=='__main__':
     deg = 3
