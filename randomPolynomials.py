3c01a81939aea7441331a6fe1bde1304009bf556
nbliss <nbliss2@uic.edu>
2017-04-11 10:29:19 -0500

    Can feed either a float % weight, or an int # monomials

--- randomPolynomials.py
+++ randomPolynomials.py
@@ -7,22 +7,33 @@ def binom(n,k):
     return prod
 
 from random import randint,random
+import random
 from TestAllPolys import GenerateAllMonomials,convertSupportToPoly
-'''
 #-------------------------------------------------------------------------------
-def randomPolySupports(DegreeBound, Dimension, numPolys=1):
+def randomPolySupports(DegreeBound, Dimension, numPolys=1, weight=0.5):
     """
-    Generates random polynomials in Dimension variables and total
-    degree <= DegreeBound. Equivalent to finding a random element of
-    the power set of all monomials, so if there are M of them, we pick
-    a random int 0<=x<=M and use its binary representation to choose
-    from the list of monomials. Can get several random polys at a time.
-    Weight is the probability that a monomial gets chosen.
+    Generates random polynomials (support) in Dimension variables and total
+    degree <= DegreeBound.
+    -- Can get several random polys at a time.
+    -- Weight is either the integer number of monomials in each polynomial
+       returned, or a float percentage of possible monomials that will occur
+       in each polynomial returned.
     """
     numberMonoms = int(binom(Dimension + DegreeBound, Dimension))
+    monomsToInclude = 0
     def getPowerSetIndex():
-        powerSetIndex = bin(randint(0,int(2**numberMonoms)))[2:]
-        return powerSetIndex.rjust(numberMonoms,'0')
+        if type(weight)==float:monomsToInclude = int(round(weight*numberMonoms))
+        elif type(weight)==int:monomsToInclude = weight
+        else:raise Exception("weight must be of type float or int")
+        if monomsToInclude<2:
+            #monomsToInclude = 2
+            raise Exception("Uh-oh. weight is so small it implies there should \
+                  be fewer than 2 monomials.")
+        psStr = ['1' for i in xrange(monomsToInclude)]
+        psStr += ['0' for i in xrange(numberMonoms-monomsToInclude)]
+        random.shuffle(psStr)
+        return ''.join(psStr)
+
     PSindices = [getPowerSetIndex() for i in xrange(numPolys)]
     monoms = GenerateAllMonomials(DegreeBound, Dimension)
     toReturn = [[] for i in xrange(numPolys)]
@@ -32,11 +43,11 @@ def randomPolySupports(DegreeBound, Dimension, numPolys=1):
             if PSindices[polyNum][index]=='1':toReturn[polyNum].append(monom)
         index+=1
     # if we just wanted one, return it,
-    # else return all of them
-    if len(toReturn)==1:return toReturn[0] 
+    # else return all of them   ##nah
+    # if len(toReturn)==1:return toReturn[0] 
     return toReturn
-'''
 
+'''
 #-------------------------------------------------------------------------------
 def randomPolySupports(DegreeBound, Dimension, numPolys=1, weight=0.5):
     """
@@ -57,6 +68,7 @@ def randomPolySupports(DegreeBound, Dimension, numPolys=1, weight=0.5):
     # else return all of them
     if len(toReturn)==1:return toReturn[0] 
     return toReturn
+'''
 
 
 if __name__=='__main__':
